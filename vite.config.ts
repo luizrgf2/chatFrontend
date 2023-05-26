b1e82f1d9e834442f800bb9d5a81d2b97d184ba1
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-26 17:13:20 -0300

    🧹 CHORE: vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import dotenv from "dotenv"
 
-dotenv.config()
-
-const {
-  VITE_WEBSOCKET_URL,
-  FRONTEND_PORT
-} = process.env
 
 
 // https://vitejs.dev/config/
@@ -18,10 +11,9 @@ export default defineConfig({
 
 
   server:{
-    port: FRONTEND_PORT === undefined? undefined : Number(FRONTEND_PORT),
     proxy: {
       "/socket.io": {
-        target: VITE_WEBSOCKET_URL,
+        target: "http://localhost:8080",
         ws: true,
       },
     }

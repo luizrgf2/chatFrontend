9e91ba1972930ac266be547bc0ef6a76afb4e9e2
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-26 13:11:27 -0300

    🛠  REFACTOR: change configration of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const {
+  VITE_WEBSOCKET_URL,
+  FRONTEND_PORT
+} = process.env
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),reactRefresh()],
 
+
+
   server:{
+    port: FRONTEND_PORT === undefined? undefined : Number(FRONTEND_PORT),
     proxy: {
       "/socket.io": {
-        target: "http://localhost:8080",
+        target: VITE_WEBSOCKET_URL,
         ws: true,
       },
     }
